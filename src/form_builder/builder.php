c020e5a9509b9efb23d26a6ff999cb6018a66d86
Gargano, Matthew <mgargano@penguinrandomhouse.com>
2017-07-25 21:01:38 -0400

    escape output of form, add in novalidate option

--- src/form_builder/builder.php
+++ src/form_builder/builder.php
@@ -16,7 +16,8 @@ class Builder {
 		$this->slug     = $slug;
 		$this->fields   = $fields;
 		$defaults       = [
-			'method' => 'POST'
+			'method'     => 'POST',
+			'novalidate' => false,
 		];
 		$this->settings = array_merge( $defaults, $settings );
 		$this->check_fields();
@@ -40,11 +41,21 @@ class Builder {
 	public function output() {
 
 
-		$before_template = '<form method="{{METHOD}}" class="{{CSS_PREFIX}}__form {{CSS_PREFIX}}__form-{{SLUG}} {{CLASS}}">';
+		$before_template = '<form method="{{METHOD}}" class="{{CSS_PREFIX}}__form {{CSS_PREFIX}}__form-{{SLUG}} {{CLASS}}" {{NOVALIDATE}}>';
 		$after_template  = '</form>';
+		$novalidate = '';
+		if ( $this->settings['novalidate'] ) {
+			$novalidate = 'novalidate="novalidate"';
+		}
 
-		$before = str_replace( [ '{{CSS_PREFIX}}', '{{SLUG}}', '{{METHOD}}', '{{CLASS}}' ],
-			[ $this->slug, $this->slug, $this->settings['method'], $this->settings['class'] ], $before_template );
+		$before = str_replace( [ '{{CSS_PREFIX}}', '{{SLUG}}', '{{METHOD}}', '{{CLASS}}', '{{NOVALIDATE}}' ],
+			[
+				esc_attr( $this->slug ),
+				esc_attr( $this->slug ),
+				esc_attr( $this->settings['method'] ),
+				esc_attr( $this->settings['class'] ),
+				esc_attr( $novalidate ),
+			], $before_template );
 		$after  = $after_template;
 
 		echo $before;
